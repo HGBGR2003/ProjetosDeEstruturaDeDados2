313cca88b3c15f133fd8e9a8b68bb17e259f34a5
Henrique Gabriel <henriquegb152019@gmail.com>
2025-04-02 23:01:05 -0300

    Nova Atividade

---
+++ Projeto 2 - Utilizando Tabela Hash/tabelahashexercicio/src/main/java/br/com/gomide/hashing/arquivo_principal/Principal.java
@@ -0,0 +1,13 @@
+package br.com.gomide.hashing.arquivo_principal;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        ArrayList listaDeAlunos = new ArrayList<Aluno>();
+        for (int i = 0; i < 15; i++) {
+            Aluno alunoPrin = new Aluno ();
+            listaDeAlunos.add(alunoPrin);
+        }
+    }
+}
